b6a90c20cc5581d55038c7775131b51daba37737
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-11-15 12:04:26 +0000

    started traying with single episode

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
+const main = document.getElementById("root");
+const episodeCard = document.querySelector(".card");
+const episodeTitle = document.getElementById("title");
+const episodeImg = document.getElementById("image");
+const episodeSum = document.getElementById("summary");
+const seasonNum = document.getElementById("seriNum");
+const episodeNum = document.getElementById("epNum");
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // const rootElem = document.getElementById("root");
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach(function (episode) {
+    episodeTitle.textContent = episode.name;
+    seasonNum.textContent = episode.season;
+    seasonNum.textContent = episode.number;
+    episodeImg.setAttribute("src", episode.image.medium);
+    episodeSum.textContent = episode.summary;
+  });
 }
 
 window.onload = setup;
