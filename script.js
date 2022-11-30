5687f10e8842c3fe7357f110a32d50d219b93ab5
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-11-30 22:30:44 +0000

    added div with innerHTML for search section

--- script.js
+++ script.js
@@ -6,46 +6,54 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  episodeList.map(function (episode,index) {
-    const main = document.getElementById("root");
+  // episodeList.map(function (episode,index) {
+  //   const main = document.getElementById("root");
+const main  = document.getElementById("root");
+main.innerHTML =`<div id="search-episodes">
+  <span class="search-bar">Search episodes</span>
+  <input type="search" class="search-episodes" placeholder="Search ...">
+  </div>`;
 
+    const container = document.createElement("div");
+    container.id = "cardContainer";
 
-    
-   
-    const episodeCard = document.createElement("div");
-    episodeCard.className = "card";
+     episodeList.forEach(function (episode, index) {
+       const episodeCard = document.createElement("div");
+       episodeCard.className = "card";
 
-    const cardHead = document.createElement("div");
-    cardHead.id = "cardHead";
-    episodeCard.appendChild(cardHead);
+       const cardHead = document.createElement("div");
+       cardHead.id = "cardHead";
+       episodeCard.appendChild(cardHead);
 
-    const episodeTitle = document.createElement("h3");
-    episodeTitle.id = "name";
-    cardHead.appendChild(episodeTitle);
+       const episodeTitle = document.createElement("h3");
+       episodeTitle.id = "name";
+       cardHead.appendChild(episodeTitle);
 
-    const seasonNum = document.createElement("h4");
-    seasonNum.id = "seriNum";
-    episodeTitle.appendChild(seasonNum);
+       const seasonNum = document.createElement("h4");
+       seasonNum.id = "seriNum";
+       episodeTitle.appendChild(seasonNum);
 
-    const episodeImg = document.createElement("img");
-    episodeImg.id = "image";
-    episodeCard.appendChild(episodeImg);
+       const episodeImg = document.createElement("img");
+       episodeImg.id = "image";
+       episodeCard.appendChild(episodeImg);
 
-    const episodeSum = document.createElement("p");
-    episodeSum.id = "summary";
-    episodeCard.appendChild(episodeSum);
+       const episodeSum = document.createElement("p");
+       episodeSum.id = "summary";
+       episodeCard.appendChild(episodeSum);
 
-    episodeTitle.textContent = episode.name;
-    // seasonNum.textContent = episode.season;
-    // seasonNum.textContent = episode.number;
-    seasonNum.textContent = `S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
-    episodeImg.setAttribute("src", episode.image.medium);
-    episodeSum.textContent = episode.summary;
-    episodeCard.id = "card" + index;
-    main.appendChild(episodeCard);
-  });
+       episodeTitle.textContent = episode.name;
+       // seasonNum.textContent = episode.season;
+       // seasonNum.textContent = episode.number;
+       seasonNum.textContent = `S${episode.season
+         .toString()
+         .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+       episodeImg.setAttribute("src", episode.image.medium);
+       episodeSum.textContent = episode.summary;
+       episodeCard.id = "card" + index;
+      //  main.appendChild(episodeCard);
+      container.appendChild(episodeCard);
+      main.appendChild(container);
+     });
 }
 
 
